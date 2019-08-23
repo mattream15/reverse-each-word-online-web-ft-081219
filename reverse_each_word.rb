624009d18914fd9872669733201e0cde0f98c80e
Matthew Ream <matthewtimothyream@gmail.com>
2019-08-23 15:31:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,13 @@
 require "pry"
-def reverse_each_word(string)
-  string = " "
-  string_array = string.split
-  interpolated_array = [ ]
-  reversed_string = " "
-    string_array.each do |word|
-      binding.pry
-    interpolated_array << #{word}.reverse
-  reversed_string = interpolated_array.join
-end
-return reversed_string
-end
+# def reverse_each_word(string)
+#   string = " "
+#   string_array = string.split
+#   interpolated_array = [ ]
+#   reversed_string = " "
+#     string_array.each do |word|
+#       binding.pry
+#     interpolated_array << #{word}.reverse
+#   reversed_string = interpolated_array.join
+# end
+# return reversed_string
+# end
