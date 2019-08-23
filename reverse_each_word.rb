f93728802fe8f3cd9eeee4afc35029f35ee3ea95
Matthew Ream <matthewtimothyream@gmail.com>
2019-08-23 15:34:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ require "pry"
 def reverse_each_word(string)
   string_array = string.split(" ")
   string_array.each do |word|
-  binding.pry
-end
+    binding.pry
+  end
 end
 
 # def reverse_each_word(string)
